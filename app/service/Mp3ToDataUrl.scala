71e25d65f9493ba1b86407c4446136c176310028
Gregor Ihmor <gregor.ihmor@gmail.com>
2021-03-18 03:26:31 +0100

    use lib to build data urls

--- app/service/Mp3ToDataUrl.scala
+++ app/service/Mp3ToDataUrl.scala
@@ -1,6 +1,12 @@
 package service
 
+import io.lemonlabs.uri.{DataUrl, MediaType}
+
 object Mp3ToDataUrl {
   def apply(mp3: Seq[Byte]): String =
-    s"data:audio/mpeg;base64,${java.util.Base64.getEncoder.encodeToString(mp3.toArray)}"
+    DataUrl(
+      mediaType = MediaType(rawValue = Some("audio/mpeg"), parameters = Vector.empty),
+      base64 = true,
+      data = mp3.toArray
+    ).toStringPunycode
 }
